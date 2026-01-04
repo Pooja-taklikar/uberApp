c71b1179e506010319449dc3191e4c2406205d3e
Pooja <poojataklikar0@gmail.com>
2026-01-04 17:01:14 +0530

    Email configuration and added prod application property

---
+++ src/main/java/com/codingshuttle/uber/uberApp/services/EmailSenderService.java
@@ -0,0 +1,8 @@
+package com.codingshuttle.uber.uberApp.services;
+
+public interface EmailSenderService {
+
+    public void sendEmail(String toEmail,String subject,String body);
+
+    public void sendEmail(String toEmail[],String subject,String body);
+}
