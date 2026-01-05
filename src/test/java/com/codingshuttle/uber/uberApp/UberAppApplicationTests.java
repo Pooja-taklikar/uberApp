2654ab885d105915cbddb56638415db7464f1045
Pooja <poojataklikar0@gmail.com>
2026-01-05 07:58:34 +0530

    Testcases commited

--- src/test/java/com/codingshuttle/uber/uberApp/UberAppApplicationTests.java
+++ src/test/java/com/codingshuttle/uber/uberApp/UberAppApplicationTests.java
@@ -8,29 +8,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UberAppApplicationTests {
 
-	@Autowired
-	private EmailSenderService emailSenderService;
-
-	@Test
-	void contextLoads() {
-		emailSenderService.sendEmail(
-				"naxatic933@emaxasp.com",
-				"This is the testing email",
-				"Body of my email");
-	}
-
-	@Test
-	void sentEmailMultiple() {
-		String emails[] = {
-				"naxatic933@emaxasp.com",
-				"poojataklik0@gmail.com",
-				"taklikarpooja11@gmail.com",
-				"ptaklikar980@gmail.com"
-		};
-		emailSenderService.sendEmail(
-				emails,
-				"This is the testing email",
-				"Hello from uber demo application");
-	}
+//	@Autowired
+//	private EmailSenderService emailSenderService;
+//
+//	@Test
+//	void contextLoads() {
+//		emailSenderService.sendEmail(
+//				"naxatic933@emaxasp.com",
+//				"This is the testing email",
+//				"Body of my email");
+//	}
+//
+//	@Test
+//	void sentEmailMultiple() {
+//		String emails[] = {
+//				"naxatic933@emaxasp.com",
+//				"poojataklik0@gmail.com",
+//				"taklikarpooja11@gmail.com",
+//				"ptaklikar980@gmail.com"
+//		};
+//		emailSenderService.sendEmail(
+//				emails,
+//				"This is the testing email",
+//				"Hello from uber demo application");
+//	}
 
 }
